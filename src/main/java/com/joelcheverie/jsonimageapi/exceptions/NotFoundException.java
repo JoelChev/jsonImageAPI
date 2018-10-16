13bc779d5b6f900459971a0f9f35a9532ab1954e
Joel Cheverie <joelcheverie@hotmail.com>
2018-10-15 23:23:02 -0400

    Added request syntax

---
+++ src/main/java/com/joelcheverie/jsonimageapi/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.joelcheverie.jsonimageapi.exceptions;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/joelcheverie/jsonimageapi/services/JSONPlaceHolderService.java
+++ src/main/java/com/joelcheverie/jsonimageapi/services/JSONPlaceHolderService.java
@@ -1,10 +1,20 @@
 package com.joelcheverie.jsonimageapi.services;
 
+import com.joelcheverie.jsonimageapi.exceptions.NotFoundException;
 import com.joelcheverie.jsonimageapi.models.Album;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
+
+import static org.springframework.http.HttpHeaders.USER_AGENT;
 
 @Service
 public class JSONPlaceHolderService {
@@ -16,7 +26,47 @@ public class JSONPlaceHolderService {
 
     public Album getAlbum() {}
 
-    private String makeRequest() {
-        
+    private String doGET(String url) throws IOException {
+        HttpGet request = new HttpGet(url);
+        return executeRequest(request);
+    }
+
+    private String doPOST(String url) throws IOException {
+        HttpPost request = new HttpPost(url);
+        return executeRequest(request);
+    }
+
+    private String doPUT(String url) throws IOException {
+        HttpPut request = new HttpPut(url);
+        return executeRequest(request);
+    }
+
+    private String doDELETE(String url) throws IOException {
+        HttpDelete request = new HttpDelete(url);
+        return executeRequest(request);
+    }
+
+    private String executeRequest(HttpRequestBase request) throws IOException {
+        HttpClient client = new DefaultHttpClient();
+        // add request header
+        request.addHeader("User-Agent", USER_AGENT);
+
+        HttpResponse response = client.execute(request);
+
+        int responseStatusCode = response.getStatusLine().getStatusCode();
+        if(responseStatusCode == 404) {
+            throw new NotFoundException("Could not find photos!");
+        }
+
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
+
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+
+        return result.toString();
     }
 }
